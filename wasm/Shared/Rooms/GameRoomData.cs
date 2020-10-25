43b26cbd3c39d40bb1b1bd56cc292185c637862f
Niklas Paulsen <npau@informatik.uni-kiel.de>
2020-10-25 22:12:22 +0100

    moves game creation to own page, prepares for adding players

--- wasm/Shared/Rooms/GameRoomData.cs
+++ wasm/Shared/Rooms/GameRoomData.cs
@@ -4,6 +4,9 @@ namespace BlazorSignalRApp.Shared.Rooms
 {
     public class GameRoomData
     {
+        public const int MinPlayers = 3;
+        public const int MaxPlayers = 6;
+
         [Required]
         [StringLength(24)]
         public string Id { get; set; }
@@ -11,6 +14,21 @@ namespace BlazorSignalRApp.Shared.Rooms
         [Required]
         [Range(3, 6)]
         public int PlayerCount { get; set; } = 3;
+
+        [Required]
+        [MinLength(MinPlayers)]
+        [MaxLength(MaxPlayers)]
+        public GamePlayerData[] Players { get; set; }
+    }
+
+    public class GamePlayerData
+    {
+        [Required]
+        [StringLength(24)]
+        public string Name { get; set; }
+
+        public bool IsHuman { get; set; }
+
     }
 
 }
